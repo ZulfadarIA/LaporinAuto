597bb4f627edb610dea5407d24e1368fd9508996
ZulfadarIA <zulfadaralkaf@gmail.com>
2025-10-24 07:32:30 +0700

    adding topbar

--- app/src/main/java/com/zulfadar/laporinauto/data/remote/connection/ApiService.kt
+++ app/src/main/java/com/zulfadar/laporinauto/data/remote/connection/ApiService.kt
@@ -2,7 +2,12 @@ package com.zulfadar.laporinauto.data.remote.connection
 
 import com.zulfadar.laporinauto.data.remote.response.AllReportResponseItem
 import com.zulfadar.laporinauto.data.remote.response.ListVehicleResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface ApiService {
@@ -13,4 +18,13 @@ interface ApiService {
 
     @GET("list_vehicle")
     suspend fun getListVehicle(): ListVehicleResponse
+
+    @Multipart
+    @POST("add_laporan")
+    suspend fun addLaporan(
+        @Part("vehicleId") vehicleId: RequestBody,
+        @Part("note") note: RequestBody,
+        @Part("userId") userId: RequestBody,
+        @Part photo: MultipartBody.Part? = null
+    )
 }
--- app/src/main/java/com/zulfadar/laporinauto/ui/screen/reportlist/ReportListScreen.kt
+++ app/src/main/java/com/zulfadar/laporinauto/ui/screen/reportlist/ReportListScreen.kt
@@ -1,22 +1,32 @@
 package com.zulfadar.laporinauto.ui.screen.reportlist
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -24,14 +34,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.zulfadar.laporinauto.R
 import com.zulfadar.laporinauto.data.remote.response.AllReportResponseItem
 import com.zulfadar.laporinauto.ui.screen.common.UiState
 import com.zulfadar.laporinauto.ui.screen.reportlist.component.HomeBottomBar
 import com.zulfadar.laporinauto.ui.screen.reportlist.component.ReportCardItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReportListScreen(
     modifier: Modifier = Modifier,
@@ -40,9 +55,57 @@ fun ReportListScreen(
     val reportState by viewModel.reportListState.collectAsState(initial = UiState.Loading)
 
     Scaffold(
-        modifier = modifier,
         topBar = {
-
+            Surface(
+                modifier = Modifier.height(230.dp),
+                shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp),
+                color =Color(0xFFFFF6BC),
+                shadowElevation = 4.dp
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Row(
+                        modifier = Modifier.padding(vertical = 12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.img),
+                            contentDescription = "Logo App",
+                            modifier = Modifier.size(40.dp)
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                        IconButton(
+                            onClick = {},
+                            colors = IconButtonDefaults.iconButtonColors(
+                                containerColor = Color.White,
+                                contentColor = Color.Black
+                            )
+                        ) {
+                            Icon(Icons.Default.Search, contentDescription = "Search")
+                        }
+                    }
+                    Spacer(Modifier.height(24.dp))
+                    Column {
+                        Text(
+                            "Laporan Keluhan Kendaraan",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                        Text(
+                            "Zulfadar Indaka Alkaf",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            }
         },
         bottomBar = {
             HomeBottomBar(
@@ -70,7 +133,7 @@ fun ReportListScreen(
                     Log.d("ReportListScreen", "Jumlah laporan: ${reports.size}")
                     if (reports.isNotEmpty()) {
                         ReportList(
-                            modifier = Modifier,
+                            modifier = Modifier.padding(top = 12.dp),
                             reportList = reports
                         )
                     } else {
